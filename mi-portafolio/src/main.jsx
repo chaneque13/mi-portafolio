cf32621d4592ea180eef43c522060b3641d34cd4
Kayden <jveduig@gmail.com>
2025-10-04 00:36:15 -0600

    fix: cambiar a HashRouter para GitHub Pages

--- mi-portafolio/src/main.jsx
+++ mi-portafolio/src/main.jsx
@@ -1,14 +1,14 @@
 // main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'  // ← Cambia de Test de vuelta a App
+import { HashRouter } from 'react-router-dom'  // ← CAMBIA A HashRouter
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />  {/* ← Aquí también */}
-    </BrowserRouter>
+    <HashRouter>  {/* ← CAMBIA A HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 )
